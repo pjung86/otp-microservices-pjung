05a4f3fa0e35741b92f7d75d26568ee42b1830ff
pjung86 <peter.jung.codecool@gmail.com>
2024-03-03 00:11:43 +0100

    add client service class

---
+++ otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/ClientService.java
@@ -0,0 +1,29 @@
+package com.pjung.coreservice.service;
+
+import com.pjung.coreservice.model.Client;
+import com.pjung.coreservice.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getAllClients() {
+        return  clientRepository.findAll();
+    }
+
+
+    public Client getClientById (Long id) {
+        return clientRepository.getClientById(id);
+    }
+}
