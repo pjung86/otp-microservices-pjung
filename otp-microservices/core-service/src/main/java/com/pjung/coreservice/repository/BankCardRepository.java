cdb05bba69f3fd5fa5b6261b4702afed456865db
pjung86 <peter.jung.codecool@gmail.com>
2024-03-03 00:13:52 +0100

    add bank card repository class

---
+++ otp-microservices/core-service/src/main/java/com/pjung/coreservice/repository/BankCardRepository.java
@@ -0,0 +1,9 @@
+package com.pjung.coreservice.repository;
+
+import com.pjung.coreservice.model.ClientBankCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankCardRepository extends JpaRepository<ClientBankCard, Long> {
+
+    ClientBankCard getClientBankCardByClientId(Long id);
+}
