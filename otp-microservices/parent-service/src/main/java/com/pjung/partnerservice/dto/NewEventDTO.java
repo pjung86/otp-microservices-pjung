58e4d1f664dd7e1fde910d840624898b5e3267c9
pjung86 <peter.jung.codecool@gmail.com>
2024-03-03 23:45:50 +0100

    update type of start and end time stamp of NewEventDTO class

--- otp-microservices/parent-service/src/main/java/com/pjung/partnerservice/dto/NewEventDTO.java
+++ otp-microservices/parent-service/src/main/java/com/pjung/partnerservice/dto/NewEventDTO.java
@@ -2,7 +2,8 @@ package com.pjung.partnerservice.dto;
 
 import com.pjung.partnerservice.model.Seat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-public record NewEventDTO (String title, String location, String startTimeStamp, String endTimeStamp, List<NewSeatDTO> seats) {
+public record NewEventDTO (String title, String location, LocalDateTime startTimeStamp, LocalDateTime endTimeStamp, List<NewSeatDTO> seats) {
 }
--- otp-microservices/ticket-service/src/main/java/com/pjung/ticketservice/dto/NewEventDTO.java
+++ otp-microservices/ticket-service/src/main/java/com/pjung/ticketservice/dto/NewEventDTO.java
@@ -1,6 +1,7 @@
 package com.pjung.ticketservice.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-public record NewEventDTO(String title, String location, String startTimeStamp, String endTimeStamp, List<NewSeatDTO> seats) {
+public record NewEventDTO(String title, String location, LocalDateTime startTimeStamp, LocalDateTime endTimeStamp, List<NewSeatDTO> seats) {
 }
