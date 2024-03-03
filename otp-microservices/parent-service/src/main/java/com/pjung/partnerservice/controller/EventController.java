1cd66a88d74959c2b70eae9f8e671ca99a24fa30
pjung86 <peter.jung.codecool@gmail.com>
2024-03-03 23:42:22 +0100

    add exceptions

--- otp-microservices/parent-service/src/main/java/com/pjung/partnerservice/controller/EventController.java
+++ otp-microservices/parent-service/src/main/java/com/pjung/partnerservice/controller/EventController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pjung.partnerservice.dto.NewEventDTO;
 import com.pjung.partnerservice.dto.ReservationDTO;
+import com.pjung.partnerservice.exceptions.SeatIsTakenException;
+import com.pjung.partnerservice.exceptions.SeatNotFoundException;
 import com.pjung.partnerservice.model.Event;
 import com.pjung.partnerservice.model.Seat;
 import com.pjung.partnerservice.repository.ReservationRepository;
@@ -20,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/events")
@@ -41,39 +44,25 @@ public class EventController {
     }
 
     @GetMapping(value = "getEvents")
-    public List<Event> getEvents () {
+    public List<Event> getEvents() {
         return eventService.getAllEvents();
     }
 
-    @GetMapping (value = "getEvent/{id}")
-    @ResponseBody
-    public ResponseEntity<Event> getEventById (@PathVariable("id") Long id) {
-        try {
-            return ResponseEntity.ok(eventService.getEventById(id));
-
-        } catch (NoSuchElementException e) {
-            HttpStatus httpStatus = HttpStatus.valueOf(90001);
-            return  new ResponseEntity(httpStatus);
-        }
+    @GetMapping(value = "getEvent/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(eventService.getEventById(id));
     }
+
     @Transactional
     @PostMapping(value = "reserve")
-    public ResponseEntity<Object> reserveSeat (@RequestParam Long eventId, @RequestParam Long seatId, @RequestBody ReservationDTO reservationDTO) {
+    public ResponseEntity<Object> reserveSeat(@RequestParam Long eventId, @RequestParam Long seatId, @RequestBody ReservationDTO reservationDTO) {
         Event event = eventService.getEventById(eventId);
-        if (event == null) {
-            return ResponseEntity.badRequest().body("Nem létezik ilyen esemény!");
-        }
 
-        Seat seat = event.getSeats().stream().filter(seat1 -> seat1.getSeatId().equals(seatId)).findFirst().orElseThrow(NoSuchElementException::new);
-        System.out.println(seat.toString());
-        if (seat == null) {
-            return ResponseEntity.badRequest().body("Nem létezik ilyen szék!");
-        }
+        Seat seat = event.getSeats().stream().filter(seat1 -> seat1.getSeatId().equals(seatId)).findFirst().orElseThrow(() -> new SeatNotFoundException("Nem létezik ilyen szék!"));
 
         if (seat.isReserved()) {
-            return ResponseEntity.badRequest().body("Már foglalt székre nem lehet jegyet foglalni!");
+            throw new SeatIsTakenException("Már lefoglalt székre nem lehet jegyet eladni!");
         }
-
 
         // Return the success response
         return ResponseEntity.ok(reservationService.addReservation(reservationDTO));
@@ -83,7 +72,8 @@ public class EventController {
     public List<Event> addEventFromJson() {
         try {
             InputStream inputStream = new ClassPathResource("getEvents.json").getInputStream();
-            List<NewEventDTO> events = objectMapper.readValue(inputStream, new TypeReference<List<NewEventDTO>>() {});
+            List<NewEventDTO> events = objectMapper.readValue(inputStream, new TypeReference<List<NewEventDTO>>() {
+            });
             List<Event> addedEvents = eventService.addEvents(events);
             return ResponseEntity.ok(addedEvents).getBody();
         } catch (IOException e) {
