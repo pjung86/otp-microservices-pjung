303ac6589a117f647ac086c44d7f94d1211feb4c
pjung86 <peter.jung.codecool@gmail.com>
2024-03-03 23:40:53 +0100

    test Base64 encoding and decoding

--- otp-microservices/core-service/src/main/java/com/pjung/coreservice/CoreServiceApplication.java
+++ otp-microservices/core-service/src/main/java/com/pjung/coreservice/CoreServiceApplication.java
@@ -1,5 +1,6 @@
 package com.pjung.coreservice;
 
+import com.pjung.coreservice.util.Base64Utility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class CoreServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreServiceApplication.class, args);
-	}
 
+		Base64Utility base64Utility = new Base64Utility();
+
+		String encoded = base64Utility.Base64Encoder("teszt.cecilia@otpmobil.com&3000&E68560872BDB2DF2FFE7ADC091755378");
+		System.out.println("Encoded string " + encoded);
+
+		String decoded = base64Utility.Base64Decoder(encoded);
+		System.out.println("Decoded string: " + decoded);
+	}
 }
