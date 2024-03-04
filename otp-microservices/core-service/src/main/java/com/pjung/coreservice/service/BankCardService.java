63b11e21007c8b3d603d93d252a1e4a0f73119b9
pjung86 <peter.jung.codecool@gmail.com>
2024-03-04 18:16:22 +0100

    add two boolean methods

--- otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/BankCardService.java
+++ otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/BankCardService.java
@@ -1,5 +1,7 @@
 package com.pjung.coreservice.service;
 
+import com.pjung.coreservice.exceptions.BankCardIsNotClientsException;
+import com.pjung.coreservice.exceptions.InsufficientFundException;
 import com.pjung.coreservice.model.ClientBankCard;
 import com.pjung.coreservice.repository.BankCardRepository;
 import com.pjung.coreservice.repository.ClientRepository;
@@ -17,6 +19,22 @@ public class BankCardService {
     }
 
     public ClientBankCard getClientBankCardById (Long id) {
-        return bankCardRepository.getClientBankCardByClientId(id);
+        return bankCardRepository.getClientBankCardById(id);
+    }
+
+    public boolean isItMyCard (Long cardId, Long clientId) {
+        ClientBankCard currentCard = bankCardRepository.getClientBankCardById(cardId);
+        if(!currentCard.isItMine(clientId)) {
+            throw new BankCardIsNotClientsException("Ez a bankkártya nem ehhez a felhasználóhoz tartozik");
+        }
+        return currentCard.isItMine(clientId);
+    }
+
+    public boolean isCardBalanceEnough (Long cardId, int amount) {
+        ClientBankCard currentCard = bankCardRepository.getClientBankCardById(cardId);
+        if(!currentCard.checkFunds(amount)) {
+            throw new InsufficientFundException("A felhasználónak nincs elegendő pénze hogy megvásárolja a jegyet!");
+        }
+        return currentCard.checkFunds(amount);
     }
 }
