39664bad5889b98aa4e96c97468796a52aa5fc0f
pjung86 <peter.jung.codecool@gmail.com>
2024-03-05 02:28:18 +0100

    add two methods for adding dummy data to DB

--- otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/ClientService.java
+++ otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/ClientService.java
@@ -1,10 +1,13 @@
 package com.pjung.coreservice.service;
 
+import com.pjung.coreservice.dto.ClientDTO;
 import com.pjung.coreservice.model.Client;
 import com.pjung.coreservice.repository.ClientRepository;
+import com.pjung.coreservice.service.builder.ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,10 +15,12 @@ import java.util.UUID;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private  final ClientBuilder clientBuilder;
 
     @Autowired
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository, ClientBuilder clientBuilder) {
         this.clientRepository = clientRepository;
+        this.clientBuilder = clientBuilder;
     }
 
     public List<Client> getAllClients() {
@@ -25,5 +30,19 @@ public class ClientService {
 
     public Client getClientById (Long id) {
         return clientRepository.getClientById(id);
+    }
+
+    public List<Client> addClients(List<ClientDTO> clients) {
+        List<Client> clientList = new ArrayList<>();
+        for (ClientDTO client : clients) {
+            Client addedEvent = addClient(client);
+            clientList.add(addedEvent);
+        }
+        return clientList;
+    }
+
+    public Client addClient(ClientDTO client) {
+
+        return clientBuilder.clientBuilder(client);
     }
 }
