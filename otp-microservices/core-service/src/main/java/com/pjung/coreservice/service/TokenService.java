6262869658b6cda3bfab145a97e4964be59256fa
pjung86 <peter.jung.codecool@gmail.com>
2024-03-05 02:24:27 +0100

    add validateToken and addToken methods

--- otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/TokenService.java
+++ otp-microservices/core-service/src/main/java/com/pjung/coreservice/service/TokenService.java
@@ -1,19 +1,29 @@
 package com.pjung.coreservice.service;
 
+import com.pjung.coreservice.dto.ClientDeviceDTO;
+import com.pjung.coreservice.dto.ClientTokenDTO;
 import com.pjung.coreservice.exceptions.TokenMissingException;
+import com.pjung.coreservice.model.ClientDevice;
 import com.pjung.coreservice.model.ClientToken;
 import com.pjung.coreservice.repository.TokenRepository;
+import com.pjung.coreservice.service.builder.ClientTokenBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TokenService {
 
     private final TokenRepository tokenRepository;
 
+    private final ClientTokenBuilder clientTokenBuilder;
+
     @Autowired
-    public TokenService(TokenRepository tokenRepository) {
+    public TokenService(TokenRepository tokenRepository, ClientTokenBuilder clientTokenBuilder) {
         this.tokenRepository = tokenRepository;
+        this.clientTokenBuilder = clientTokenBuilder;
     }
 
     public ClientToken getTokenByToken(String token) {
@@ -22,5 +32,24 @@ public class TokenService {
             throw new TokenMissingException("A felhasználói token nem szerepel");
         }
         return clientToken;
+    }
+
+    public boolean validateToken (String token) {
+        ClientToken currentToken = getTokenByToken(token);
+        System.out.println("This is token from DB: " + currentToken);
+        return currentToken.getToken().equals(token);
+    }
+
+    public List<ClientToken> addTokens(List<ClientTokenDTO> clientTokenDTOS) {
+        List<ClientToken> clientTokenList = new ArrayList<>();
+        for (ClientTokenDTO clientTokenDTO : clientTokenDTOS) {
+            ClientToken addedClientToken = addToken(clientTokenDTO);
+            clientTokenList.add(addedClientToken);
+        }
+        return clientTokenList;
+    }
+
+    public ClientToken addToken(ClientTokenDTO clientTokenDTO) {
+        return clientTokenBuilder.tokenBuilder(clientTokenDTO);
     }
 }
