00cca481a5c73b7b4c7e21a7d7549bc0da3ba2e7
pjung86 <peter.jung.codecool@gmail.com>
2024-03-05 17:10:30 +0100

    add resttemplate instead of webclient

--- otp-microservices/api-gateway/src/main/java/com/pjung/apigateway/service/ApiService.java
+++ otp-microservices/api-gateway/src/main/java/com/pjung/apigateway/service/ApiService.java
@@ -2,24 +2,22 @@ package com.pjung.apigateway.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class ApiService {
 
-    private final WebClient.Builder webClientBuilder;
-
-    @Autowired
-    public ApiService(WebClient.Builder webClientBuilder) {
-        this.webClientBuilder = webClientBuilder;
+    public ApiService() {
     }
 
-    public boolean validateToken(String token) {
-        boolean result = webClientBuilder.build().get()
-                .uri("http://core-service/core/getToken/{token}", token)
-                .retrieve()
-                .bodyToMono(Boolean.class)
-                .block();
+    public boolean validateToken(String token) throws ExecutionException, InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+        Boolean result = restTemplate.getForObject("http://localhost:8080/core/validateToken/" + token, Boolean.class);
+
         return result;
     }
 }
